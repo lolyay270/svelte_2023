91fd70e7e6752c2ca85d649481ea9687d889d88d
Jenna Boyes (1000056739) <BOYEJE2@student.op.ac.nz>
2023-08-04 11:59:48 +1200

    linked new font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,15 @@
 
 <svelte:head>
     <link rel="stylesheet" href="css/main.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kalam:wght@700&display=swap" rel="stylesheet">
 </svelte:head>
 
 <Header />
 
 <slot />
+
+<img src="img/mega.jpg" alt="megamind meme" />
 
 <Footer />
