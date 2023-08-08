69beffbe31ba42c9f74fb3de4205e31fbe81b219
Jenna Boyes (1000056739) <BOYEJE2@student.op.ac.nz>
2023-08-09 09:11:01 +1200

    did css exercise in class

--- src/routes/other/+page.svelte
+++ src/routes/other/+page.svelte
@@ -1,1 +1,17 @@
 <h1> hi it other page </h1>
+
+<div class="main">
+    <h1>Main heading</h1>
+    <p>
+    	This is some informative content.
+    </p>
+    <ul>
+      <li>One</li>
+      <li>Two</li>
+      <li>Three</li>
+      <li>Four</li>
+    </ul>
+    <div class="footer">
+    	Footer content
+    </div>
+</div>
