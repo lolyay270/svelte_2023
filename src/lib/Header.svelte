765d0eded9b4950fde09ec97cc3f17e55df13e28
Jenna Boyes (1000056739) <BOYEJE2@student.op.ac.nz>
2023-09-01 11:16:42 +1200

    added cute fish text icons

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -4,5 +4,8 @@
 
 <header>
     <Nav />
+    <i class="fa-solid fa-fish-fins"></i>
+    <i class="fa-solid fa-fish-fins"></i>
+    <i class="fa-solid fa-fish-fins"></i>
     <h1 class="logo">Fishie Facts</h1>
 </header>
