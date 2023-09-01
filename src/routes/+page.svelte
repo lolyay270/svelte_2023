caebfd17b09c38b22238f113ca97bb4387a87cb9
Jenna Boyes (1000056739) <BOYEJE2@student.op.ac.nz>
2023-09-01 12:02:41 +1200

    added sub header that relates to the grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <h1>Welcome to the website that will poorly tell you about fish</h1>
-
+<h2>Here are some examples you can find on my website:</h2>
 <div class="home_blocks">
     <div>a</div>
     <div>b</div>
