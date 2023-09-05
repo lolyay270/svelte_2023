cf8b32f010c771565db5e4d4b0380c1aed2f97de
Jenna Boyes (1000056739) <BOYEJE2@student.op.ac.nz>
2023-09-06 09:54:18 +1200

    added div for background img, and content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <h1>Welcome to the website that will poorly tell you about fish</h1>
 <h2>Here are some examples you can find on my website:</h2>
 <div class="home_blocks">
-    <div>a</div>
-    <div>b</div>
+    <div>
+        <div class="img_blocks"></div>
+        <p>Discus</p>
+    </div>
+    <div><img src="" /></div>
     <div>c</div>
     <div>d</div>
 </div>
