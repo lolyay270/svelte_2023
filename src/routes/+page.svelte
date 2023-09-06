59a25c6f6675fb6cc97e63857fd5824dca49eab9
Jenna Boyes (1000056739) <BOYEJE2@student.op.ac.nz>
2023-09-06 13:47:54 +1200

    changed the heading level for the content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<h1>Welcome to the website that will poorly tell you about fish</h1>
-<h2>Here are some examples you can find on my website:</h2>
+<h2>Welcome to the website that will poorly tell you about fish</h2>
+<h3>Here are some examples you can find on my website:</h3>
 <div class="home_blocks">
     <div> 
         <div class="img_blocks discus2"></div>
