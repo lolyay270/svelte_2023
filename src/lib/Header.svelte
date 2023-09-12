e8d7d47df6760f327e6868612fed13b76f6ba669
Jenna Boyes (1000056739) <BOYEJE2@student.op.ac.nz>
2023-09-13 09:32:50 +1200

    made header sticky using scroll in JS

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,8 +1,17 @@
 <script>
     import Nav from "$lib/Nav.svelte";
+    import { onMount } from "svelte";
+
+    let isSticky = false;
+    onMount(() => {
+        document.addEventListener("wheel", (event) => {            
+            isSticky = event.deltaY < 0;                     
+        });
+    })
+    
 </script>
 
-<header>
+<header class={isSticky ? 'sticky' : 'notSticky'}>
     <Nav />
     <div class="fishes">
         <i class="fa-solid fa-fish-fins"></i>
