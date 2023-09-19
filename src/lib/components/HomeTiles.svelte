1e2667a537751d788fe6f668e114d17a88a3c672
Jenna Boyes (1000056739) <BOYEJE2@student.op.ac.nz>
2023-09-20 09:56:25 +1200

    tried to use JS to loop through the tiles on home, it doesnt work

--- src/lib/components/HomeTiles.svelte
+++ src/lib/components/HomeTiles.svelte
@@ -1,3 +1,7 @@
 <script>
-    
+    export let name;
+    export let url_link;
 </script>
+
+<div class="img_blocks" style="background-image: url({url_link})"></div>
+<p>{name}</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,17 @@
+<script>
+    import HomeTiles from "../lib/components/HomeTiles.svelte";
+    let fishes = [
+        {"name": "Discus", "url_link": "../img/discus2.jpg"},
+        {"name": "Royal Blue Tang", "url_link": "../img/blue2.jpg"},
+        {"name": "Betta", "url_link": "../img/betta3.jpg"},
+        {"name": "Goldfish", "url_link": "../img/gold3.jpg"}
+    ]
+</script>
+
 <h2>Welcome to the website that will poorly tell you about fish</h2>
 <h3>Here are some examples you can find on my website:</h3>
 <div class="home_blocks">
-    <div> 
-        <div class="img_blocks discus2"></div>
-        <p>Discus</p>
-    </div>
-    <div>
-        <div class="img_blocks blue2"></div>
-        <p>Royal Blue Tang</p> 
-    </div>
-    <div>
-        <div class="img_blocks betta3"></div>
-        <p>Betta</p>
-    </div>
-    <div>
-        <div class="img_blocks gold3"></div>
-        <p>Goldfish</p>
-    </div>
+    {#each fishes as fish}
+        <HomeTiles name="{fish}", url_link="{url_link}" />
+    {/each}
 </div>
