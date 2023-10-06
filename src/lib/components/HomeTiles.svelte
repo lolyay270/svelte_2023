e92fa8c620fa2b3467a1f34002192eb9adf07126
lolyay270 <boyeje2@student.op.ac.nz>
2023-10-06 21:19:41 +1300

    changing what api im using, pulling img urls

--- src/lib/components/HomeTiles.svelte
+++ src/lib/components/HomeTiles.svelte
@@ -1,7 +1,20 @@
 <script>
-    export let name;
-    export let url_link;
+    const base_url = `https://api.thecatapi.com/v1`
+const api_key = `live_yBWH0otnDLXMNNVIjdb7j6vV0F5tMFOUcbbOJ4mNDcKWnOdscPe99331duAJQ4kL`
+
+    fetch(`${base_url}/images/search?limit=4&has_breeds=0&breed_ids=beng&api_key=${api_key}`)
+    .then((response) => response.json())
+    .then((data) => {
+        data.forEach(cat => {
+          console.log(cat.url)
+        })
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+
+    //list of breed codes i want to use
+    //beng      birm        bomb        drex        mcoo        norw        rblu        tang
+    //
 </script>
 
-<div class="img_blocks" style="background-image: url({url_link})"></div>
-<p>{name}</p>
