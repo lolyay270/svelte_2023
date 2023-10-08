d52ddf5b655d44e40f1ccf4bfb1cc14c87f9dad9
lolyay270 <boyeje2@student.op.ac.nz>
2023-10-08 22:06:58 +1300

    copied JS from HomeTiles to home page, altered foreach to cats, removed unneeded fish code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,38 @@
 <script>
-    import HomeTiles from "../lib/components/HomeTiles.svelte";
-    let fishes = [
-        {"name": "Discus", "url_link": "../img/discus2.jpg"},
-        {"name": "Royal Blue Tang", "url_link": "../img/blue2.jpg"},
-        {"name": "Betta", "url_link": "../img/betta3.jpg"},
-        {"name": "Goldfish", "url_link": "../img/gold3.jpg"}
-    ]
+
+    import { onMount } from "svelte";
+
+    let cats = [];
+
+    const base_url = `https://api.thecatapi.com/v1`
+    const api_key = `live_yBWH0otnDLXMNNVIjdb7j6vV0F5tMFOUcbbOJ4mNDcKWnOdscPe99331duAJQ4kL`
+
+    onMount(() => {
+        fetch(`${base_url}/images/search?limit=4&breed_ids=beng,birm,bomb,drex,mcoo,norw,rblu,tang&api_key=${api_key}`)
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach(cat => { 
+                cats.push(cat)
+                console.log(cat)
+            })
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+        console.log(cats)
+    })
+
+    //https://api.thecatapi.com/v1/images/search?limit=4&breed_ids=beng,birm,bomb,drex,mcoo,norw,rblu,tang&api_key=live_yBWH0otnDLXMNNVIjdb7j6vV0F5tMFOUcbbOJ4mNDcKWnOdscPe99331duAJQ4kL
 </script>
 
 <h2>Welcome to the website that will poorly tell you about fish</h2>
 <h3>Here are some examples you can find on my website:</h3>
 <div class="home_blocks">
-    {#each fishes as fish}
-        <HomeTiles name="{fish}", url_link="{url_link}" />
+    {#each cats as cat}
+        <div>
+            <div class="img_blocks" style="background-image: url({cat.url})"></div>
+            <p>{cat.breeds[0].name}</p>
+            <p> hi am words, pls work</p>
+        </div>
     {/each}
 </div>
