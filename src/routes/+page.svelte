4fb8d87b491d0bec85dd8b26a33886d7054a3289
Jenna Boyes (1000056739) <BOYEJE2@student.op.ac.nz>
2023-10-27 12:03:59 +1300

    adding notation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
     const BASE_URL = `https://api.thecatapi.com/v1`
     const api_key = `live_yBWH0otnDLXMNNVIjdb7j6vV0F5tMFOUcbbOJ4mNDcKWnOdscPe99331duAJQ4kL`
 
+    //onmount for getting 4 random cat imgs from specific cat ids 
     onMount(async () => { 
         let res = await fetch(`${BASE_URL}/images/search?limit=4&breed_ids=beng,birm,bomb,drex,mcoo,norw,rblu,tang&api_key=${api_key}`); 
     	cats = await res.json();
@@ -14,6 +15,7 @@
     //https://api.thecatapi.com/v1/images/search?limit=4&breed_ids=beng,birm,bomb,drex,mcoo,norw,rblu,tang&api_key=live_yBWH0otnDLXMNNVIjdb7j6vV0F5tMFOUcbbOJ4mNDcKWnOdscPe99331duAJQ4kL
 </script>
 
+<!-- showing img and name for each cat in array -->
 <div class="home_blocks">
     {#each cats as cat}
         <div>
