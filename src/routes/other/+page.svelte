6ec98a239d5f2204c6a9bed7e9ecd4fc2f73eb36
Jenna Boyes (1000056739) <BOYEJE2@student.op.ac.nz>
2023-10-27 12:22:23 +1300

    trying to loop thru fetching with different feilds

---
+++ src/routes/other/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onMount } from "svelte";
+    const obj = {
+        url: `url here`,
+        id: `id here`,
+    };
+    let cats = [];
+
+    const BASE_URL = `https://api.thecatapi.com/v1`
+    const API_KEY = `live_yBWH0otnDLXMNNVIjdb7j6vV0F5tMFOUcbbOJ4mNDcKWnOdscPe99331duAJQ4kL`
+    const CAT_IDS = [`beng`, `birm`, `bomb`, `drex`, `mcoo`, `norw`, `rblu`, `tang`]
+
+    //onmount for looping thru each id i want, then filling an array of objects with the url and id 
+    onMount(async () => { 
+        CAT_IDS.forEach(id, i => {
+            let res = await fetch(`${BASE_URL}/images/search?limit=5&breed_ids=${id}&api_key=${API_KEY}`); 
+            let data = await res.json();
+            obj.url = data.url;
+            obj.id = id;
+            cats.push(obj);
+        });
+    })
+</script>
