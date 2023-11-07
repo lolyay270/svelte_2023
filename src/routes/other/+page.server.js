649a57affc1e0c4fd174b79f14bf6a930aee560b
Jenna Boyes (1000056739) <BOYEJE2@student.op.ac.nz>
2023-11-07 14:52:23 +1300

    fixed fetch with map and Promise.all, refactored code to be more tidy

--- src/routes/other/+page.server.js
+++ src/routes/other/+page.server.js
@@ -1,31 +1,50 @@
-export async function load({fetch}){
-    
-    const BASE_URL = `https://api.thecatapi.com/v1`
-    const API_KEY = `live_yBWH0otnDLXMNNVIjdb7j6vV0F5tMFOUcbbOJ4mNDcKWnOdscPe99331duAJQ4kL`
-    const CAT_IDS = [`beng`, `birm`, `bomb`, `drex`, `mcoo`, `norw`, `rblu`, `tang`]
-    // https://api.thecatapi.com/v1/images/search?breed_ids=beng&api_key=live_yBWH0otnDLXMNNVIjdb7j6vV0F5tMFOUcbbOJ4mNDcKWnOdscPe99331duAJQ4kL
-    let cats = [];
-    
-    CAT_IDS.forEach(async id => {
-        // let cats [] = cats;
-        let res = await fetch(`${BASE_URL}/images/search?limit=5&breed_ids=${id}&api_key=${API_KEY}`); 
-        let data = await res.json();
-        // console.log(data);
-        let obj = {
-            urls: [],
-            id: `id here`,
-            breed: `breed here`,
-        };
-        data.forEach(i => {
-            obj.urls.push(i.url);
-            obj.breed = i.breeds[0].name;   
-            obj.id = id;
-        });
-        cats.push(obj);
-        console.log(obj);
+export async function load() {
+  const BASE_URL = "https://api.thecatapi.com/v1";
+  const API_KEY = "live_yBWH0otnDLXMNNVIjdb7j6vV0F5tMFOUcbbOJ4mNDcKWnOdscPe99331duAJQ4kL";
+  const CAT_IDS = [
+    "beng",
+    "birm",
+    "bomb",
+    "drex",
+    "mcoo",
+    "norw",
+    "rblu",
+    "tang",
+  ];
+
+  let cats = [];
+
+  //fetch takes each id from CAT_IDS and gets 5 images and maps them to obj with id and breed
+  const fetchData = async (id) => {
+    const res = await fetch(
+      `${BASE_URL}/images/search?limit=5&breed_ids=${id}&api_key=${API_KEY}`
+    );
+    const data = await res.json();
+
+    let obj = {
+      urls: [],
+      id, //is shorthand for id: id,
+      breed: data[0].breeds[0].name,
+    };
+
+    data.forEach((i) => {
+      obj.urls.push(i.url);
     });
-    
-    console.log(cats)
-    
-    return {cats};
+    cats.push(obj);
+  };
+
+
+
+  //I am using Promise.all to get only 1 response for all fetches for each cat id
+
+  //google search: "how to fetch many times at once javascript"
+  //opened: https://stackoverflow.com/questions/50381326/really-run-fetch-multiple-times-synchronously
+  //which linked to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+
+  //given by Elise
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+  await Promise.all(CAT_IDS.map((id) => fetchData(id)));
+
+  return { cats };
 }
