7da85a08ad12ab0b80add31257ec66dd60b3d7cd
lolyay270 <boyeje2@student.op.ac.nz>
2023-11-07 19:02:15 +1300

    removed unneeded code

--- src/routes/other/+page.svelte
+++ src/routes/other/+page.svelte
@@ -13,17 +13,4 @@
     {/each}
 </div>
 
-
-
-<div class="not_this_one"> <!-- container for component -->
-    <h2></h2> <!-- cat name big when hover color white, other h2 color grey -->
-    <div> <!-- container for imgs (hide until hover h2)-->
-        <img />
-        <img /> 
-        <img /> <!-- all imgs from fetch -->
-        <img /> 
-        <img /> 
-    </div>
-</div>
-
 <!-- inspiration:    https://www.coulisse-gallery.com/artists -->
