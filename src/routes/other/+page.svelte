c8cf39939da4b869a630e4f991c63a9eba406272
lolyay270 <boyeje2@student.op.ac.nz>
2023-11-07 20:39:01 +1300

    made names large, imgs hide unless hover, hover and background tweaks

--- src/routes/other/+page.svelte
+++ src/routes/other/+page.svelte
@@ -1,16 +1,23 @@
 <script>
     export let data;
     console.log(data.cats);
-    // console.log(`length = ${data.cats.length}`)
 </script>
 
-<div class="contain">
+<div class="container">
     {#each data.cats as cat}
+        <div class="catGallery">
             <h2>{cat.breed}</h2>
-            {#each cat.urls as url}
-                <img src="{url}" alt="{cat.breed}"/>
-            {/each}
+            <div class="gallery">
+                {#each cat.urls as url}
+                    <img src={url} alt={cat.breed} />
+                {/each}
+            </div>
+        </div>
     {/each}
 </div>
 
 <!-- inspiration:    https://www.coulisse-gallery.com/artists -->
+
+<style>
+
+</style>
